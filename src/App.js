ef63494e23063849730876acd98eaee2023b33bd
Austin Wang <austinpower1258@gmail.com>
2022-01-14 23:50:15 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 function App() {
+  const { width, height } = useWindowSize()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Confetti
+      width={width}
+      height={height}
+      />
+      <div class="card">
+        <div class="outside">
+            <div class="front">
+                <p>Happy Birthday AUSTIN W!</p>
+                <div class="cake">
+                    <div class="top-layer"></div>
+                    <div class="middle-layer"></div>
+                    <div class="bottom-layer"></div>
+                    <div class="candle"></div>
+                </div>
+            </div>
+            <div class="back"></div>
+        </div>
+        <div class="inside">
+            <p>You're old now! You still suck at coding.</p>
+            <h1>&#127873;</h1>
+        </div>
     </div>
+    </div>
+
+    
+
+ 
+
   );
 }
 
+
 export default App;
+
+
